678e2cd90b45e6e3c099d954e0f96c3a47d0550b
seanluse41 <seanluse41@gmail.com>
2024-10-14 15:27:34 +0900

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.css";
+  const { children } = $props();
+
+</script>
+
+<div class="flex flex-col min-h-screen bg-whitesmoke relative p-12">
+  {@render children()}
+</div>
+
+<style lang="postcss">
+  :global(body) {
+    background-color: cornsilk;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let loginState = $state(false);
+
+    let tryLogin = () => {
+        console.log("trying login")
+    }
+</script>
+
+<h1 class="text-3xl font-bold">Kintone Oauth 2.0</h1>
+<button
+    class="text-xl bg-red-600 text-white w-1/4 self-center py-5 rounded hover:bg-red-500"
+    onclick={tryLogin}
+    >Login with Kintone</button
+>
+
+<p>Login Status: {loginState}</p>
